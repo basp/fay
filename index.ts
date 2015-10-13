132c982c2530b11a7a1c3a3e05595419134c0133
Bas Pennings <baspster@gmail.com>
2015-10-13 21:59:04 +0200

    Ok this is going not too bad

--- index.ts
+++ index.ts
@@ -2,21 +2,28 @@
 
 import $ = require('jquery');
 
+function execute(input: string) {
+	return [0];
+}
+
 $(() => {
-	var $cmd = $('.chibi-command');
+	var $cmd = $('.command');
+	var $console = $('.console');
 	
     const ENTER = 13,
           UP = 38,
           DOWN = 40;
-	
+
 	$cmd.keydown(x => {
 		switch (x.which) {
-			case ENTER:
+			case ENTER: {
 				x.preventDefault();
-				var input = $cmd.get(0).innerText;
-				console.log(input);
-				$cmd.text('');				
+				let input = $cmd.get(0).innerText;
+				let result = execute(input);
+				console.log(result);
+				$cmd.text('');
  				return false;
+			}
 			case UP:
 				return false;
 			case DOWN:
